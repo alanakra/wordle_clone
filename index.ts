1bcef008b9dffda558e59ecfd11ce37a60187947
Alan Akra <alanakra@hotmail.fr>
2022-12-11 18:18:42 +0100

    Dark

--- index.ts
+++ index.ts
@@ -139,6 +139,7 @@ function checkGuess () {
 function fillTable (letter: string, position: number) {
     console.warn(`Letter: ${letter}. ArrRightGuessSubstring: ${arrRightGuessSubstring[position]}. Type: ${typeof(arrRightGuessSubstring[position]!)}. Index: ${arrRightGuessSubstring.indexOf(arrRightGuessSubstring[position]!)}`)
     const row = document.getElementsByClassName('letter-row') as HTMLCollectionOf<Element>
+    row[6 - guessesRemaining]!.childNodes[position]!.style.color = "black"
     if (arrRightGuessSubstring.includes(letter)) {
         if (letter === arrRightGuessSubstring[position]) {
             console.log(`Check position: ${position} ${arrRightGuessSubstring.indexOf(arrRightGuessSubstring[position]!)}`)
